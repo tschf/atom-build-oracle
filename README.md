d38c74df4f93aeb1ec7248e535aa72b0b1efe8ae
Trent Schafer <trent.schafer@gmail.com>
2015-11-24 14:15:29 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Oracle compiler for Atom
+
+Uses the [atom-build](https://atom.io/packages/build) package to execute Oracle compilations in the Atom editor.
+
+This package requires [atom-build](https://atom.io/packages/build) to be installed.
---
+++ lib/atom_compileOracle.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+if [[ "$#" -ne 6 ]]
+then
+    echo "Invalid number of args"
+    echo "Received $# expected 6"
+    exit 1
+fi
+
+exit 2
+echo "Running"
+echo "host: $1"
+echo "port: $2"
+echo "sid: $3"
+echo "user: $4"
+# echo "password: $5"
+echo "Compiling file: $6"
+
+sqlplus -S -L $4/$5@$1:$2/$3 << EOF
+
+    set linesize 200
+    set tab off
+    set serveroutput on
+
+    COLUMN MY_USER FORMAT A20
+    COLUMN DB FORMAT A20
+    COLUMN SID FORMAT A10
+    COLUMN NOW FORMAT A35
+    --Show the details of the connection for confirmation
+    select
+        user as MY_USER
+      , ora_database_name as DB
+      --, '$3' as SID
+      , systimestamp as NOW
+    from dual;
+
+    @$6
+
+    COLUMN MY_USER 	CLEAR
+    COLUMN DB 	    CLEAR
+    COLUMN SID 	    CLEAR
+    COLUMN NOW 	    CLEAR
+
+    show error
+
+EOF

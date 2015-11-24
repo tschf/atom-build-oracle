0bd2bb7225d7a79f053e071db86bd698b94c2c67
Trent Schafer <trent.schafer@gmail.com>
2015-11-24 19:14:06 +1100

    Call script with args instead of input redirect Before, the compilation was embedded in the shell script, redirecting input until it came accross an EOF indicator.
    sqlplus -S -L $4/$5@$1:$2/$3 << EOF
    
    script input here
    
    EOF
    
    Switching to have a compilation SQL script and call directly

---
+++ lib/compile.sql
@@ -0,0 +1,27 @@
+set linesize 200
+set tab off
+set serveroutput on
+
+COLUMN MY_USER FORMAT A20
+COLUMN DB FORMAT A20
+COLUMN SID FORMAT A10
+COLUMN NOW FORMAT A35
+
+--Show the details of the connection for confirmation
+    select
+        user as MY_USER
+      , ora_database_name as DB
+      --, '$3' as SID
+      , systimestamp as NOW
+    from dual;
+
+@&1
+
+COLUMN MY_USER 	CLEAR
+COLUMN DB 	    CLEAR
+COLUMN SID 	    CLEAR
+COLUMN NOW 	    CLEAR
+
+show error
+
+exit
--- lib/compileOracle.sh
+++ lib/compileOracle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ "$#" -ne 6 ]]
+if [[ "$#" -ne 7 ]]
 then
     echo "Invalid number of args"
     echo "Received $# expected 6"
@@ -14,32 +14,6 @@ echo "sid: $3"
 echo "user: $4"
 # echo "password: $5"
 echo "Compiling file: $6"
+#echo "Compile script: $7"
 
-sqlplus -S -L $4/$5@$1:$2/$3 << EOF
-
-    set linesize 200
-    set tab off
-    set serveroutput on
-
-    COLUMN MY_USER FORMAT A20
-    COLUMN DB FORMAT A20
-    COLUMN SID FORMAT A10
-    COLUMN NOW FORMAT A35
-    --Show the details of the connection for confirmation
-    select
-        user as MY_USER
-      , ora_database_name as DB
-      --, '$3' as SID
-      , systimestamp as NOW
-    from dual;
-
-    @$6
-
-    COLUMN MY_USER 	CLEAR
-    COLUMN DB 	    CLEAR
-    COLUMN SID 	    CLEAR
-    COLUMN NOW 	    CLEAR
-
-    show error
-
-EOF
+sqlplus $4/$5@$1:$2/$3 @$7 $6

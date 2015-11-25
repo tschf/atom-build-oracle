ef1885c29fdbf42c1fb1da378fb7b8cab21627f2
Trent Schafer <trent.schafer@gmail.com>
2015-11-26 09:38:55 +1100

    Add error checking for specified path. Closes #8

--- lib/compileOracle.bat
+++ lib/compileOracle.bat
@@ -10,4 +10,23 @@ REM echo Compile script: %7
 echo sql interpreter: %8
 )
 
-%8 %4/%5@%1:%2/%3 @%6 %7
+SET SUCCESS_CODE=0
+SET fileFound=-1
+SET fileLoc=%8
+
+REM if where finds the path specified, the exit code will be 0
+REM this is useful when only specifying a basename
+REM (for a simple name that will resolve in %path%)
+where %fileLoc% > NUL 2>&1
+SET whereCode=%ERRORLEVEL%
+if %whereCode% equ %SUCCESS_CODE% set fileFound=%SUCCESS_CODE%
+
+REM otherwise (full path) the location should return true with the exist function
+if exist %fileLoc% set fileFound=%SUCCESS_CODE%
+
+if %fileFound% neq %SUCCESS_CODE% (
+    echo The specified SQL interpreter ^(%fileLoc%^) could not be found
+    exit /B 1
+)
+
+%fileLoc% %4/%5@%1:%2/%3 @%6 %7
--- lib/compileOracle.sh
+++ lib/compileOracle.sh
@@ -2,8 +2,8 @@
 
 if [[ "$#" -ne 8 ]]
 then
-    echo "Invalid number of args"
-    echo "Received $# expected 8"
+    echo "Invalid number of args" >&2
+    echo "Received $# expected 8" >&2
     exit 1
 fi
 
@@ -17,6 +17,11 @@ echo "Compiling file: $7"
 #echo "Compile script: $7"
 echo "Interpreter path: $8"
 
+INTERPRER_PATH=$(which $8)
 
+if [[ ! -x $INTERPRER_PATH ]]; then
+    echo "$8 either can not be found on the system, or is not executable" >&2
+    exit 2
+fi
 
 $8 $4/$5@$1:$2/$3 @$6 $7

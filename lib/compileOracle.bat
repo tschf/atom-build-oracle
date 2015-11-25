4f97c576980399069823b0aad2e740088fb90424
Trent Schafer <trent.schafer@gmail.com>
2015-11-25 21:20:47 +1100

    Add windows build script. Closes #1

---
+++ lib/compileOracle.bat
@@ -0,0 +1,12 @@
+@echo off
+(echo Running
+echo host: %1
+echo port: %2
+echo sid: %3
+echo user: %4
+REM echo password: %5
+REM echo Compiling file: %6
+REM echo Compile script: %7
+)
+
+C:\sqlcl\bin\sql.bat %4/%5@%1:%2/%3 @%6 %7

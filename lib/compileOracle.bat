f4ecf7ad611c630e494d6f04f627561c03cf5398
Trent Schafer <trent.schafer@gmail.com>
2015-11-26 12:12:14 +1100

    Replace numbered vars with named vars. Closes #6

--- lib/compileOracle.bat
+++ lib/compileOracle.bat
@@ -1,32 +1,52 @@
 @echo off
-(echo Running
-echo host: %1
-echo port: %2
-echo sid: %3
-echo user: %4
-REM echo password: %5
-REM echo Compiling file: %6
-REM echo Compile script: %7
-echo sql interpreter: %8
-)
+REM Passed in arguments:
+SET RECEIVED_ARGS=$#
+SET EXPECTED_ARGS=8
+SET HOST=%1
+SET PORT=%2
+SET SID=%3
+SET USER=%4
+SET PASSWORD=%5
+SET COMPILE_SCRIPT=%6
+SET SQL_CODE_FILE=%7
+SET SQL_INTERPRETER=%8
 
+REM Display an output of the above variables for debugging
+REM CALL :print_debug
+
+REM used to test the return code of where to see if the file exists
 SET SUCCESS_CODE=0
+
+REM fileFound will be set to 0 if it exists. Set to anything else (-1) for now
 SET fileFound=-1
-SET fileLoc=%8
 
 REM if where finds the path specified, the exit code will be 0
 REM this is useful when only specifying a basename
 REM (for a simple name that will resolve in %path%)
-where %fileLoc% > NUL 2>&1
+where %SQL_INTERPRETER% > NUL 2>&1
 SET whereCode=%ERRORLEVEL%
 if %whereCode% equ %SUCCESS_CODE% set fileFound=%SUCCESS_CODE%
 
-REM otherwise (full path) the location should return true with the exist function
-if exist %fileLoc% set fileFound=%SUCCESS_CODE%
+REM otherwise the location (absolute path) should return true with the exist function
+if exist %SQL_INTERPRETER% set fileFound=%SUCCESS_CODE%
 
 if %fileFound% neq %SUCCESS_CODE% (
-    echo The specified SQL interpreter ^(%fileLoc%^) could not be found
+    echo The specified SQL interpreter ^(%SQL_INTERPRETER%^) could not be found
     exit /B 1
 )
 
-%fileLoc% %4/%5@%1:%2/%3 @%6 %7
+%SQL_INTERPRETER% %USER%/%PASSWORD%@%HOST%:%PORT%/%SID% @%COMPILE_SCRIPT% %SQL_CODE_FILE%
+
+
+exit /B 0
+
+:print_debug
+echo host: %HOST%
+echo port: %PORT%
+echo sid: %SID%
+echo user: %USER%
+echo password: %PASSWORD%
+echo Compile script: %COMPILE_SCRIPT%
+echo Compiling file: %SQL_CODE_FILE%
+echo Interpreter path: %SQL_INTERPRETER%
+exit /B 0
--- lib/compileOracle.sh
+++ lib/compileOracle.sh
@@ -1,27 +1,44 @@
 #!/bin/bash
+#compileOracle.sh: Simple script to run an SQL script in the arguments specified
+#Build for the atom package `build-oracle` so Oracle developers are able to compile
+#their scripts using the build system.
+RECEIVED_ARGS=$#
+EXPECTED_ARGS=8
+HOST=$1
+PORT=$2
+SID=$3
+USER=$4
+PASSWORD=$5
+COMPILE_SCRIPT=$6
+SQL_CODE_FILE=$7
+SQL_INTERPRETER=$8
 
-if [[ "$#" -ne 8 ]]
+function print_debug {
+    echo "host: ${HOST}"
+    echo "port: ${PORT}"
+    echo "sid: ${SID}"
+    echo "user: ${USER}"
+    echo "password: ${PASSWORD}"
+    echo "Compile script: ${COMPILE_SCRIPT}"
+    echo "Compiling file: ${SQL_CODE_FILE}"
+    echo "Interpreter path: ${SQL_INTERPRETER}"
+}
+
+if [[ "${RECEIVED_ARGS}" -ne ${EXPECTED_ARGS} ]]
 then
     echo "Invalid number of args" >&2
-    echo "Received $# expected 8" >&2
+    echo "Received ${RECEIVED_ARGS} expected ${EXPECTED_ARGS}" >&2
     exit 1
 fi
 
-echo "Running"
-echo "host: $1"
-echo "port: $2"
-echo "sid: $3"
-echo "user: $4"
-# echo "password: $5"
-echo "Compiling file: $7"
-#echo "Compile script: $7"
-echo "Interpreter path: $8"
+#print_debug
 
-INTERPRER_PATH=$(which $8)
 
-if [[ ! -x $INTERPRER_PATH ]]; then
-    echo "$8 either can not be found on the system, or is not executable" >&2
+INTERPRER_PATH=$(which ${SQL_INTERPRETER})
+
+if [[ ! -x ${INTERPRER_PATH} ]]; then
+    echo "${SQL_INTERPRETER} either can not be found on the system, or is not executable" >&2
     exit 2
 fi
 
-$8 $4/$5@$1:$2/$3 @$6 $7
+${SQL_INTERPRETER} ${USER}/${PASSWORD}@${HOST}:${PORT}/${SID} @${COMPILE_SCRIPT} ${SQL_CODE_FILE}

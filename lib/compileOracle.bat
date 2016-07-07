1a6781bf4ff9f0cce4865fcb349ccab5f828dd7c
Trent Schafer <trent.schafer@gmail.com>
2016-07-07 15:08:25 +1000

    Updated so windows compilation works. Introduced a series of enhancements for *nix based systems to enable SQLcl to run in the background, which broke the windows compilation process. Closes #23 #24

--- lib/compileOracle.bat
+++ lib/compileOracle.bat
@@ -2,14 +2,25 @@
 REM Passed in arguments:
 SET RECEIVED_ARGS=$#
 SET EXPECTED_ARGS=8
+
 SET HOST=%1
 SET PORT=%2
 SET SID=%3
 SET USER=%4
 SET PASSWORD=%5
-SET COMPILE_SCRIPT=%6
+SET PKG_DIR=%6
 SET SQL_CODE_FILE=%7
 SET SQL_INTERPRETER=%8
+
+REM Multi word argument comes with double quotes. When concatenating, it ends up
+REM with concatenated value outside the quotes
+REM (e.g "C:\Users\trent\.atom\dev\packages\build-oracle"\lib\compileSimple.sql).
+REM So, need to remove the quotes before concat and then re-add. Idea taken
+REM from: http://stackoverflow.com/a/5181182/3476713.
+REM I'm sure there's a better approach?
+
+SET FULL_PATH_COMPILE_SCRIPT=%PKG_DIR:"=%
+SET FULL_PATH_COMPILE_SCRIPT="%FULL_PATH_COMPILE_SCRIPT%\lib\compileSimple.sql"
 
 REM Display an output of the above variables for debugging
 REM CALL :print_debug
@@ -35,7 +46,7 @@ if %fileFound% neq %SUCCESS_CODE% (
     exit /B 1
 )
 
-%SQL_INTERPRETER% %USER%/%PASSWORD%@%HOST%:%PORT%/%SID% @%COMPILE_SCRIPT% %SQL_CODE_FILE%
+%SQL_INTERPRETER% -L %USER%/%PASSWORD%@%HOST%:%PORT%/%SID% @%FULL_PATH_COMPILE_SCRIPT% %SQL_CODE_FILE%
 
 
 exit /B 0
@@ -46,7 +57,7 @@ echo port: %PORT%
 echo sid: %SID%
 echo user: %USER%
 echo password: %PASSWORD%
-echo Compile script: %COMPILE_SCRIPT%
+echo Compile script: %FULL_PATH_COMPILE_SCRIPT%
 echo Compiling file: %SQL_CODE_FILE%
 echo Interpreter path: %SQL_INTERPRETER%
 exit /B 0
---
+++ lib/compileSimple.sql
@@ -0,0 +1,21 @@
+set linesize 200
+
+set serveroutput on
+set verify off
+
+COLUMN MY_USER FORMAT A20
+COLUMN DB FORMAT A15
+COLUMN NOW FORMAT A40
+
+--Show the details of the connection for confirmation
+select user MY_USER, ora_database_name DB, systimestamp NOW from dual;
+
+@&1
+
+COLUMN MY_USER 	CLEAR
+COLUMN DB 	    CLEAR
+COLUMN NOW 	    CLEAR
+
+show error
+
+exit

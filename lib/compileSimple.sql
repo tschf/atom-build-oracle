f20b85c6f6585efecd8fad692cd756a0812e434d
Trent Schafer <trent.schafer@gmail.com>
2016-07-29 17:14:28 +1000

    Add more information to compile output

--- lib/compileSimple.sql
+++ lib/compileSimple.sql
@@ -7,9 +7,14 @@ COLUMN MY_USER FORMAT A20
 COLUMN DB FORMAT A15
 COLUMN NOW FORMAT A40
 
+prompt Database connection information
+
 --Show the details of the connection for confirmation
 select user MY_USER, ora_database_name DB, systimestamp NOW from dual;
 
+prompt
+prompt Compiling script: &1
+prompt 
 @"&1"
 
 COLUMN MY_USER 	CLEAR
